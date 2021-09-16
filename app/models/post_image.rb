7db8b2afe26fca2356fb6d66d7742af571276088
nsdakt <dnj.0077@gmail.com>
2021-09-16 12:39:26 +0000

    [finish]4-6

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,9 @@ class PostImage < ApplicationRecord
   attachment :image #画像アップ用メソッド（refileを使用するためのルール）
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
