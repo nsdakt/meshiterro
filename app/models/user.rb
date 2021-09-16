6f7bc41bd5573e5f975587773abc05a421dbafd7
nsdakt <dnj.0077@gmail.com>
2021-09-16 08:31:12 +0000

    [finish]4-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :post_images, dependent: :destroy #投稿画像のアソシエーション
-  has_many :post_comments, dependent: :destroy
+  has_many :post_images, dependent: :destroy #投稿した画像とのアソシエーション
+  has_many :post_comments, dependent: :destroy #投稿したコメントとのアソシエーション
+  has_many :favorites, dependent: :destroy #投稿へのいいねのアソシエーション
 end
